006cbe8777d0dfabd0f517836b2c2839ba76dfa1
wjm <1563374406@qq.com>
2019-03-11 11:52:25 +0800

    第一次提交

--- Basics/LessonOne.py
+++ Basics/LessonOne.py
@@ -1,11 +1,11 @@
+# 基础语法
+
 # 标识符(变量名)
 # 第一个字符必须是字母表中字母或下划线_.
 # 标识符的其他的部分由字母、数字和下划线组成。
 # 标识符对大小写敏感。
 
-
 string = 'Runoob'
-
 print(string)  # 输出字符串
 print(string[0:-1])  # 输出第一个到倒数第二个的所有字符
 print(string[0])  # 输出字符串第一个字符
@@ -15,4 +15,35 @@ print(string * 2)  # 输出字符串两次
 print(string + '你好')  # 连接字符串
 print('hello\nrunoob')  # 使用反斜杠（\）+n转义特殊字符
 print(r'hello\nrunoob')  # 在字符串前面添加一个r，表示原始字符串，不会发生转义
+input('\n\n按下 enter 键后退出')  # "\n\n"在结果输出前会输出两个新的空行。一旦用户按下 enter 键时，程序将退出。
+
+# Python可以在同一行中使用多条语句，语句之间使用分号(;)分割
+import sys; x = 'runoob'; sys.stdout.write(x + '\n')
+
+# print 默认输出是换行的，如果要实现不换行需要在变量末尾加上 end=""：
+x = 'a'
+y = 'b'
+# 换行输出
+print(x)
+print(y)
+# 不换行输出
+print(x, end='')
+print(y, end='')
+
+# 在python用import或者from...import来导入相应的模块
+# 将整个模块（somemodule)都导入，格式为：import somemodule
+# 从某个模块中导入某个函数，格式为：from somemodule import somefunction
+# 从某个模块中导入多个函数，格式为：from somemodule import firstfunc, secondfunc, thirdfunc
+# 将某个模块中的全部函数导入，格式为：from somemodule import *
+"""导入sys模块"""
+import sys
+print('             python import mode          ')
+print('命令行参数为：')
+for i in sys.argv:
+    print(i)
+print('\n python 路径为', sys.path)
+"""导入sys模块的argv，path成员"""
+from sys import argv,path  # 导入特定的成员
+print('        python from import       ')
+print('path:', path)  # 因为已经导入path成员，所以此次引用时不需要加sys.paths
 
---
+++ Basics/data_type.py
@@ -0,0 +1,250 @@
+# 基本数据类型
+dir()  # 对象   # 例子：dir(int)
+help()  # 对象或对象＋方法   # 例子：help（int）  或者   help（int.imag）
+
+# 需要背记的方法
+int.bit_length()  # 作用是得到指定数值的二进制的长度数、宽度数
+int.conjugate()
+int.denominator  # 有理数在最低点的分母
+int.from_bytes()  # res = int.from_bytes(x)的含义是把bytes类型的变量x，转化为十进制整数，并存入res中
+int.imag()  # 复数的虚部
+int.numerator()  # 有理数最低项的分子
+int.real()  # 复数的实部
+int.to_bytes()  # 功能：是int.from_bytes的逆过程，把十进制整数，转换为bytes类型的格式。
+
+float.as_integer_ratio()
+float.conjugate()
+float.fromhex()
+float.hex()
+float.imag()  # 复数的虚部
+float.is_integer()
+float.real()  # # 复数的实部
+
+bool.bit_length()
+bool.conjugate()
+bool.denominator()
+bool.from_bytes()
+bool.imag()
+bool.numerator()
+bool.real()
+bool.to_bytes()
+
+complex.conjugate()
+complex.imag()
+complex.real()
+
+str.capitalize()
+str.casefold()
+str.center()
+str.count()
+str.encode()
+str.endswith()
+str.expandtabs()
+str.find()
+str.format()
+str.format_map()
+str.index()
+str.isalnum()
+str.isalpha()
+str.isdecimal()
+str.isdigit()
+str.isidentifier()
+str.islower()
+str.isnumeric()
+str.isprintable()
+str.isspace()
+str.istitle()
+str.isupper()
+str.join()
+str.ljust()
+str.lower()
+str.lstrip()
+str.maketrans()
+str.partition()
+str.replace()
+str.rfind()
+str.rindex()
+str.rjust()
+str.rpartition()
+str.rsplit()
+str.rstrip()
+str.split()
+str.splitlines()
+str.startswith()
+str.strip()
+str.swapcase()
+str.title()
+str.translate()
+str.upper()
+str.zfill()
+
+list.append()
+list.copy()
+list.count()
+list.extend()
+list.index()
+list.insert()
+list.pop()
+list.remove()
+list.reverse()
+list.sort()
+
+tuple.count()
+tuple.index()
+
+set.add()
+set.clear()
+set.copy()
+set.difference()
+set.difference_update()
+set.discard()
+set.intersection()
+set.intersection_update()
+set.isdisjoint()
+set.issubset()
+set.issuperset()
+set.pop()
+set.remove()
+set.symmetric_difference()
+set.symmetric_difference_update()
+set.union()
+set.update()
+
+dict.clear()
+dict.copy()
+dict.fromkeys()
+dict.get()
+dict.items()
+dict.keys()
+dict.pop()
+dict.popitem()
+dict.setdefault()
+dict.update()
+dict.values()
+
+
+# Python3 中有六个标准的数据类型：
+# Number（数字）         int float bool complex
+# String(字符串)         str
+# List(列表)             list
+# Tuple（元组）          tuple
+# Set(集合)              set
+# Dictionary(字典)       dict
+# Python3的六个标准数据类型中：
+# 不可变数据（3个）：Number（数字）、String（字符串）、Tuple（元组）
+# 可变数据（3个）：List（列表）、Dictionary（字典）、Set（集合）
+
+"""Number（数字）"""
+# python3支持int、float、bool、complex（复数）
+# 可以用isinstance和type来判断类型
+# type()不会认为子类是一种父类类型
+# isinstance()会认为子类是一种父类类型
+# 5 + 4 # 加法
+# 4.3 - 2 # 减法
+# 3 * 7 # 乘法
+# 2 / 4 # 除法，得到一个浮点数
+# 2 // 4 # 除法，得到一个整数
+# 17 % 3 # 取余
+# 2 ** 5 # 乘方
+
+# python可以同时为多个变量赋值，如：a, b = 1, 2
+# 一个变量可以通过赋值指向不同类型的对象
+# 数值的除法包含两个运算符：/返回一个浮点数，//返回一个整数
+# 在混合计算时，python会把整型转换为浮点数
+# python还支持复数，复数由实数部分和虚数部分构成，可以用a+bj，或者complex(a,b)表示，复数的实数a和虚部b都是浮点数
+
+"""String（字符串）"""
+# python中的字符串用单引号''或双引号""括起来，同时使用反斜杠\转义特殊字符
+# 字符串的截取的语法格式如下：
+# 变量[头下标:尾下标]
+# 索引值以0为开始值，-1为从末尾的开始位置
+# 从后面索引：  -6  -5  -4  -3  -2  -1
+# 从前面索引：  0    1   2   3   4   5
+#               a    b   c   d   e   f
+# 从前面截取：  :    1   2   3   4   5
+# 从后面截取：  :   -5  -4  -3  -2  -1
+# 加号+是字符串的连接符；星号*表示复制当前字符串，紧跟的数字为复制的次数；
+# python使用反斜杠（\）转义特殊字符，如果你不想让反斜杠发生转义，可以在字符串的前面添加r，表示原始字符串
+# 另外，反斜杠(\)可以作为续行符，表示下一行是上一行的延续。也可以使用 """...""" 或者 '''...''' 跨越多行。
+# 注意，Python 没有单独的字符类型，一个字符就是长度为1的字符串。
+# 与 C 字符串不同的是，Python 字符串不能被改变。向一个索引位置赋值，比如word[0] = 'm'会导致错误。
+
+"""List（列表）"""
+# List（列表）是python中使用最频繁的数据类型
+# 列表可以完成大多数集合类的数据结构实现。列表中元素的类型可以不相同，它支持数字，字符串甚至可以包含列表（所谓嵌套）。
+# 列表是写在方括号[]之间，用逗号分隔开的元素列表
+# 和字符串一样，列表同样可以被索引和截取，列表被截取后返回一个包含所需元素的新列表
+# 列表截取的语法格式如下：
+# 变量[头下标:尾下标]
+# 索引值以 0 为开始值，-1 为从末尾的开始位置。
+#         t = ['a', 'b', 'c', 'd', 'e']
+# 从前面索引：  0    1    2    3    4
+# 从后面索引： -5   -4   -3   -2   -1
+# t = [1:3]     返回  ['b', 'c']
+# t = [3:]      返回  ['d', 'e']
+# t = [:4]      返回  ['a', 'b', 'c', 'd']
+# t = [:]       返回  ['a', 'b', 'c', 'd', 'e']
+
+# 加号+是列表连接运算符，星号*是重复操作，如下实例：
+list1 = ['abcd', 786, 2.23, 'runoob', 70.2]
+tinylist = [123, 'runoob']
+print(list1)  # 输出完整列表
+print(list1[0])  # 输出列表第一个元素
+print(list1[1:3])  # 从第二个开始输出到第三个元素
+print(list1[2:])  # 输出从第三个元素开始的所有元素
+print(tinylist * 2)  # 输出两次列表
+print(list1 + tinylist)  # 连接列表
+# 与python字符串不一样的是，列表中的元素是可以改变的
+
+# python列表截取可以接收第三个参数，参数作用是截取的步长，以下实例在索引1到索引4的位置并设置为步长为2（间隔一个位置）来截取字符串：
+#   索引      0    1    2    3    4    5    6
+# letters = ['c', 'h', 'e', 'c', 'k', 'i', 'o']
+# letters[1:4:2]     返回  ['h', 'c']
+
+"""Tuple（元组）"""
+# 元组（tuple）与列表类似，不同之处在于元组的元素不能修改。元组写在小括号 () 里，元素之间用逗号隔开。
+# 元组中的元素类型也可以不相同：
+tuple = ('abcd', 786, 2.23, 'runoob', 70.2)
+tinytuple = (123, 'runoob')
+print(tuple)             # 输出完整元组
+print(tuple[0])          # 输出元组的第一个元素
+print(tuple[1:3])        # 输出从第二个元素开始到第三个元素
+print(tuple[2:])         # 输出从第三个元素开始的所有元素
+print(tinytuple * 2)     # 输出两次元组
+print(tuple + tinytuple)  # 连接元组
+# 元组与字符串类似，可以被索引且下标索引从0开始，-1 为从末尾开始的位置，也可以进行截取。
+# 虽然tuple的元素不可改变，但它可以包含可变的对象，比如list列表。
+# 构建包含0个或者1个元素的元组比较特殊，所以有一些额外的语法规则：
+tup1 = ()  # 空元组
+tup2 = (20,)  # 一个元素，需要在元素后添加逗号
+# string（字符串）、list（列表）和tuple（元组）都属于sequence（序列）
+
+"""Set（集合）"""
+# Set（集合）是由一个或数个形态各异的大小整体组成的，构建集合的事物或对象称作元素或是成员。
+# 基本功能是进行成员关系测试和删除重复元素。
+# 可以使用大括号{}或者set()函数创建集合，注意：创建一个空集合必须用set()而不是{}，因为{}是用来创建一个空字典
+# parame = {value01, value02, ...}    或者       set(value)
+# 实例：
+student = {'Tom', 'Jim', 'Mary', 'Tom', 'Jack', 'Rose'}
+print(student)  # 输出集合，重复的元素被自动去掉
+# 成员测试
+if 'Rose' in student:
+    print('Rose 在集合中')
+else:
+    print('Rose 不在集合中')
+
+# set可以进行集合运算
+a = set('abracadabra')
+b = set('alacazam')
+print(a - b)  # a和b的差集
+print(a | b)  # a和b的并集
+print(a & b)  # a和b的交集
+print(a ^ b)  # a和b中不同时存在的元素
+
+"""Dictionary（字典）"""
+# Dictionary（字典）是python中另一个非常有用的内置数据类型
+# 列表是有序的对象集合，字典是无序的对象集合，两者之间的区别在于：字典当中的元素是通过键来存取的，而不是通过便宜存取。
+# 字典是一种映射类型，字典用{}标识，它是一个无序的键(key):值(value)的集合。
+# 键（key）必须使用不可变类型。
+# 在同一个字典中，键（key）必须是唯一的。
+
