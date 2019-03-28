2534759f39e66355bac23b966854ee5b7e8daf6f
wjm <1563374406@qq.com>
2019-03-28 18:24:41 +0800

    内置函数

--- senior/built_function.py
+++ senior/built_function.py
@@ -86,7 +86,7 @@ id()  # 用于获取对象的内存地址
 sorted()  # 对所有可迭代的对象进行排序操作
 # sorted(iterable, key=None, reverse=False)  # iterable：可迭代对象  key：主要是用来进行比较的元素，只有一个参数，
 # 具体的函数的参数就是取自于可迭代对象中，指定可迭代对象中的一个元素来进行排序  reverse：排序规则，reverse = True 降序，
-# reverse = False 升序（默认）  a = [3, 4, 1, 9] reverse(a)
+# reverse = False 升序（默认）  a = [3, 4, 1, 9] sorted(a)
 
 ascii()  # 函数类似 repr() 函数, 返回一个表示对象的字符串, 但是对于字符串中的非 ASCII 字符则返回通过 repr() 函数
 # 使用 \x, \u 或 \U 编码的字符。 生成字符串类似 Python2 版本中 repr() 函数的返回值。
