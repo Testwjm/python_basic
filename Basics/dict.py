88db5d2555689243213074cd235f6b7254b20e3d
wjm <1563374406@qq.com>
2019-04-02 13:48:42 +0800

    字典

--- Basics/dict.py
+++ Basics/dict.py
@@ -59,3 +59,38 @@ print(dict6['Name'])
 # get()  # 返回指定键的值，如果值不在字典中返回默认值
 # dict.get(key, default=None)  # key：字典中要查找的键   default：如果指定键的值不存在时，返回该默认值None
 # # dict = {'name': 'run', 'age': '18', 'class': 'one'}  dict.get('age')
+
+# in 操作符用于判断键是否存在于字典中，如果键在字典 dict 里返回 true，否则返回 false。
+# 而 not in 操作符刚好相反，如果键在字典 dict 里返回 false，否则返回 true
+# key in dict  # key：要在字典中查找的键
+dict1 = {'name': 'run', 'age': '22', 'class': 'one'}
+if 'age' in dict1:
+    print('age,存在')
+else:
+    print('age,不存在')
+
+# items() # 以列表返回可遍历的(键, 值) 元组数组
+# dict.items()  # dict1 = {'name': 'run', 'age': '22', 'class': 'one'}  dict1.items()
+
+#  keys()  # 返回一个可迭代对象，可以使用 list() 来转换为列表
+# dict.keys()  # dict1 = {'name': 'run', 'age': '22', 'class': 'one'}  dict1.keys()  list(dict1.keys())
+
+#  setdefault() 方法和 get()方法 类似, 如果键不已经存在于字典中，将会添加键并将值设为默认值
+# dict.setdefault(key, default=None)  # key：查找的键值   default：键不存在时，设置的默认键值
+# dict1 = {'name': 'run', 'age': '12', 'class': 'one'}  dict1.setdefault('name')
+
+# update()  # 把字典参数 dict2 的 key/value(键/值) 对更新到字典 dict 里
+# dict.update(dict2)  # dict2：添加到指定字典dict里的字典
+# dict1 = {'name': 'run', 'age': '18'}  dict2 = {'class': 'one'}  dict1.update(dict2)
+
+# values()  # 返回一个迭代器，可以使用 list() 来转换为列表，列表为字典中的所有值
+# dict.values()  # dict1 = {'name': 'run', 'age': '18', 'class': 'one'}  list(dict1.values())
+
+#  pop()  # 删除字典给定键 key 所对应的值，返回值为被删除的值。key值必须给出。 否则，返回default值
+# pop(key[,default])  # key: 要删除的键值  default: 如果没有 key，返回 default 值
+# dict1 = {'name': 'run', 'age': '22', 'calss': 'one'}  dict1.pop('name')
+
+# popitem()  # 随机返回并删除字典中的一对键和值(一般删除末尾对)
+# 如果字典已经为空，却调用了此方法，就报出KeyError异常
+# popitem()  # dict1 = {'name': 'run', 'age': '22', 'class': 'one'}  dict1.popitem()
+
